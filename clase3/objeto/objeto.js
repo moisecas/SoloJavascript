15f2feae595b994f93d9528c8408e5c9bbe427a7
Your Name <moisesjuniorcas20@gmail.com>
2022-05-31 14:14:04 -0500

    clases y objetos

--- clase3/objeto/objeto.js
+++ clase3/objeto/objeto.js
@@ -1,15 +1,17 @@
-function Producto(nombre, precio, cantidad, iva) {
-    this.nombre = nombre; //this, su propiedad nombre, le asigna el valor de los parametros 
-    this.precio = precio; //los parametros definen los atributos del objeto 
-    this.cantidad = cantidad;
-    this.iva = iva
-    this.precioFinal = function () {
-        return this.precio * this.iva
-    }, //funcion que calcula el precio final del producto
-    this.descuentocantidad = function (unidad) {
-        this.cantidad = this.cantidad - unidad;
+class Producto {
+    constructor(nombre, precio, cantidad, iva) {
+        this.nombre = nombre; //this, su propiedad nombre, le asigna el valor de los parametros 
+        this.precio = precio; //los parametros definen los atributos del objeto 
+        this.cantidad = cantidad;
+        this.iva = iva;
+        this.precioFinal = function () {
+            return this.precio * this.iva;
+        }, //funcion que calcula el precio final del producto
+            this.descuentocantidad = function (unidad) {
+                this.cantidad = this.cantidad - unidad;
+            };
+
+
+
     }
-    
-    
-  
 }
