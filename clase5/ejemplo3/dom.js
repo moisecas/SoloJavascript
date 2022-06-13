023de52447189e3d516b51404db6ff0ae42a2c34
Your Name <moisesjuniorcas20@gmail.com>
2022-06-13 16:36:40 -0500

    ejercicio dom

---
+++ clase5/ejemplo3/dom.js
@@ -0,0 +1,36 @@
+function cambiacolor(color){
+    caja.style.background = color;
+}
+
+//conseguir elemento por id
+const caja = document.getElementById("caja") //capturar el contenido de un elemento html 
+caja.style.background = "red" //cambiar el color de fondo de un elemento html 
+caja.style.padding = "20px" //cambiar el padding de un elemento html 
+caja.style.color = "black" //cambiar el color de un elemento html 
+caja.className="hola" //cambiar la clase de un elemento html
+
+console.log(caja)
+
+//conseguir elemento por etiqueta 
+const div=document.getElementsByTagName("div") //capturar el contenido de un elemento html
+console.log(div) //muestro el contenido del elemento html 
+
+var valor 
+for(valor in div){ //itero sobre el array de elementos html
+    if(typeof div[valor].textContent=="string"){ //si el valor es un string 
+        var parrafo = document.createElement("p") //creo un elemento p para poder agregarle contenido
+        var texto = document.createTextNode(div[valor].textContent)  //pinto el contenido del elemento en el html
+        parrafo.append(texto)  
+        seccion.append(parrafo) //agrego el elemento al id seccion, le paso el elemento html que estoy creando 
+        
+    }
+}
+
+
+// const contenido = div[1].innerHTML //capturar el contenido de un elemento html
+// console.log(contenido) //muestro el contenido del elemento html 
+
+
+
+
+//conseguir elemento por clase 
