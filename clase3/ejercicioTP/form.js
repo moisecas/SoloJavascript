be9a00fe351b507ceecca23748102936424357ab
Your Name <moisesjuniorcas20@gmail.com>
2022-06-28 16:37:24 -0500

    se agrega nav, dos paginas, css

---
+++ clase3/ejercicioTP/form.js
@@ -0,0 +1,35 @@
+function capturardatosinteresado(){
+    var nombre = document.getElementById("nombre").value;
+    var id = document.getElementById("id").value;
+    var correo = document.getElementById("email").value;
+    var telefono = document.getElementById("tel").value;
+
+    
+    Swal.fire("Datos registrados: \n" + "Nombre: " + nombre + "\nId: " + id + "\nCorreo: " + correo + "\nTelefono: " + telefono)
+
+
+}
+
+
+const objdatos = {
+    nombre: nombre,
+    id: id,
+    correo: correo,
+    telefono: telefono
+}
+
+
+
+
+function verificarcredito(){
+    var salario = document.getElementById("salario").value;
+    var salarioEsperado = 1000000;
+    if(salario > salarioEsperado){
+        
+        Swal.fire("Hay viabilidad para el crédito")
+    }else{
+        Swal.fire("No hay viabilidad para el crédito, por el salario"); 
+    }
+    var boton = document.getElementById("boton");
+    boton.style.backgroundColor = "red";
+}
